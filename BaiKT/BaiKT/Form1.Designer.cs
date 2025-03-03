b31e73a990cfdf636395c3e45fd132762c56d738
Kaguya-Shinomiya <hs.quangbao@gmail.com>
2025-03-03 09:23:03 +0700

    Add label Mat khau, txtmatkhau

--- BaiKT/BaiKT/Form1.Designer.cs
+++ BaiKT/BaiKT/Form1.Designer.cs
@@ -30,6 +30,8 @@
 		{
 			lblUsername = new Label();
 			txtUserName = new TextBox();
+			label1 = new Label();
+			txtmatkhau = new TextBox();
 			SuspendLayout();
 			// 
 			// lblUsername
@@ -43,16 +45,34 @@
 			// 
 			// txtUserName
 			// 
-			txtUserName.Location = new Point(195, 138);
+			txtUserName.Location = new Point(224, 138);
 			txtUserName.Name = "txtUserName";
 			txtUserName.Size = new Size(251, 27);
 			txtUserName.TabIndex = 1;
+			// 
+			// label1
+			// 
+			label1.AutoSize = true;
+			label1.Location = new Point(104, 193);
+			label1.Name = "label1";
+			label1.Size = new Size(70, 20);
+			label1.TabIndex = 2;
+			label1.Text = "Mật khẩu";
+			// 
+			// txtmatkhau
+			// 
+			txtmatkhau.Location = new Point(224, 186);
+			txtmatkhau.Name = "txtmatkhau";
+			txtmatkhau.Size = new Size(251, 27);
+			txtmatkhau.TabIndex = 3;
 			// 
 			// Form1
 			// 
 			AutoScaleDimensions = new SizeF(8F, 20F);
 			AutoScaleMode = AutoScaleMode.Font;
 			ClientSize = new Size(800, 450);
+			Controls.Add(txtmatkhau);
+			Controls.Add(label1);
 			Controls.Add(txtUserName);
 			Controls.Add(lblUsername);
 			Name = "Form1";
@@ -65,5 +85,7 @@
 
 		private Label lblUsername;
 		private TextBox txtUserName;
+		private Label label1;
+		private TextBox txtmatkhau;
 	}
 }
