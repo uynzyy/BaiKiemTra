60e947574b6d364afffa7eb3beb1b233ea3ef5c3
uyenvy <th.uyenvy1802@gmail.com>
2025-03-03 09:19:45 +0700

    commit label, textbox username

--- BaiKT/BaiKT/Form1.Designer.cs
+++ BaiKT/BaiKT/Form1.Designer.cs
@@ -20,20 +20,50 @@
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
+		#region Windows Form Designer generated code
 
-        /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
-        /// </summary>
-        private void InitializeComponent()
-        {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
-        }
+		/// <summary>
+		///  Required method for Designer support - do not modify
+		///  the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			lblUsername = new Label();
+			txtUserName = new TextBox();
+			SuspendLayout();
+			// 
+			// lblUsername
+			// 
+			lblUsername.AutoSize = true;
+			lblUsername.Location = new Point(102, 141);
+			lblUsername.Name = "lblUsername";
+			lblUsername.Size = new Size(75, 20);
+			lblUsername.TabIndex = 0;
+			lblUsername.Text = "Username";
+			// 
+			// txtUserName
+			// 
+			txtUserName.Location = new Point(195, 138);
+			txtUserName.Name = "txtUserName";
+			txtUserName.Size = new Size(251, 27);
+			txtUserName.TabIndex = 1;
+			// 
+			// Form1
+			// 
+			AutoScaleDimensions = new SizeF(8F, 20F);
+			AutoScaleMode = AutoScaleMode.Font;
+			ClientSize = new Size(800, 450);
+			Controls.Add(txtUserName);
+			Controls.Add(lblUsername);
+			Name = "Form1";
+			Text = "Form1";
+			ResumeLayout(false);
+			PerformLayout();
+		}
 
-        #endregion
-    }
+		#endregion
+
+		private Label lblUsername;
+		private TextBox txtUserName;
+	}
 }
